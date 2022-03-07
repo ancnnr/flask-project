886d2327bece5dbcb390f2739772751678c6e8d7
ancnnr <ancnnr@gmail.com>
2022-03-06 21:15:24 -0400

    got profile picture updating properly

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -40,3 +42,26 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
 
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+                                validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField('Email', 
+                                validators=[DataRequired(), Email()])
+
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose another')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('A user already exists with this email address')    
